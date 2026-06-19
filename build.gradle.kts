02c6d9d687a17c50af79ea462ff9a5ad8a53ae88
DYK42 <yurandk42@gmail.com>
2026-06-19 15:03:37 +0700

    pre-test setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,26 @@ plugins {
 group = "ru.netology"
 version = "1.0-SNAPSHOT"
 
+// используем 11 версию языка
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+// кодировка файлов (если используете русский язык в файлах)
+tasks {
+    withType<JavaCompile>().configureEach { options.encoding = "UTF-8" }
+    withType<JavaExec>().configureEach { defaultCharacterEncoding = "UTF-8" }
+    withType<Javadoc>().configureEach { options.encoding = "UTF-8" }
+    withType<Test>().configureEach { defaultCharacterEncoding = "UTF-8" }
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("com.codeborne:selenide:7.15.0")
 }
 
 tasks.test {
